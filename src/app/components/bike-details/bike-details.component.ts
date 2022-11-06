7c85c42526a2f834cd8a652f8d476783d2387ab3
kabhaskar <kabhaskar@deloitte.com>
2022-11-06 23:03:48 +0530

    accordian display

--- src/app/components/bike-details/bike-details.component.ts
+++ src/app/components/bike-details/bike-details.component.ts
@@ -7,6 +7,28 @@ import {Bike} from "src/app/model/bike.model";
     styleUrls: ["./bike-details.component.scss"],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BikeDetailsComponent {
+export class BikeDetailsComponent implements OnChanges{
+    
     @Input() bikeDetails: Bike;
+
+    expansionStatus = {
+        img: true,
+        description: false,
+        frameDetails: false,
+        publicImages: false
+    }
+
+    getThumbnailImage() {
+        const thumbImg = "/assets/alt-thumbnail.jpeg";
+        return this.bikeDetails.thumb ? this.bikeDetails.thumb : thumbImg;
+    }
+
+    getBikeImage() {
+        const imageNotFound = "/assets/cycle-drawing.jpg";
+        return this.bikeDetails.large_img ? this.bikeDetails.large_img : imageNotFound;
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log(this.bikeDetails)
+    }
 }
